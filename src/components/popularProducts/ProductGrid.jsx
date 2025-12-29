7802d910405b15838dc5bddafc4f558cfd58be95
Rishi Kumar <aladincreater@gmail.com>
2025-12-29 13:55:28 +0530

    updated the pictures

--- src/components/popularProducts/ProductGrid.jsx
+++ src/components/popularProducts/ProductGrid.jsx
@@ -2,32 +2,36 @@ import React, { useState } from 'react';
 import Tabs from './Tabs';
 import ProductList from './ProductList';
 import './styles/ProductGrid.css';
+import product5 from "../../assets/product5.png";
+import product6 from "../../assets/product6.png";
+import product7 from "../../assets/product7.png";
+import product8 from "../../assets/product8.png";
 
 const productData = {
   'Top Rated': [
     {
       name: 'Apple Watch Series 8',
       price: 188.0,
-      image: '/src/assets/product5.png',
+      image: product5,
     },
     {
       name: 'TECLAST Tablet Protective Cover Case',
       sale: 12,
       originalPrice: 899.0,
       discountedPrice: 791.12,
-      image: '/src/assets/product6.png',
+      image: product6,
     },
     {
       name: 'ViewSonic Professional Monitor',
       sale: 6,
       originalPrice: 299.0,
       discountedPrice: 281.06,
-      image: '/src/assets/product7.png',
+      image: product7,
     },
     {
       name: 'Buy Guild Planer - 900w',
       price: 239.0,
-      image: '/src/assets/product8.png',
+      image: product8,
     },
   ],
   'Best Selling': [],
